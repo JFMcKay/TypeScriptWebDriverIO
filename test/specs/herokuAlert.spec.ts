a2b34a00d5fe413d8f08e7c2566b81d6f17575d3
Jeremy F McKay <jeremyfmckay@gmail.com>
2023-03-07 12:12:20 -0800

    Updated a bit for selenium grid

--- test/specs/herokuAlert.spec.ts
+++
@@ -1,9 +0,0 @@
-
-describe('Fill a popup', () => {
-    it('fill in alert', async () => {
-        await browser.url("https://admin:admin@the-internet.herokuapp.com/basic_auth")
-        let banner = (await $("p=Congratulations! You must have the proper credentials."));
-        console.log(banner.getText());
-        expect(banner.getText()).toEqual('Congratulations! You must have the proper credentials.');
-    });
-});
